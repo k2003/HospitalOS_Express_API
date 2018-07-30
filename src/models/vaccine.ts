804ff090fef0ec6534ce4ab20c8cf4998a36aab6
Komsant Janthasema <ik0m@K0M-MacBook-Air.local>
2018-07-30 09:50:24 +0700

    add pm2

--- src/models/vaccine.ts
+++ src/models/vaccine.ts
@@ -18,11 +18,14 @@ export class vaccineModel {
   detail(knex: Knex, id: string, limit: number = 50 , offset: number = 0) {
     return knex(this.tableName)
     .column(           
-    `t_health_epi_detail.t_patient_id,`
+    `t_health_epi_detail.t_patient_id`
     ,`b_health_epi_group.health_epi_group_description as vacc,`
-    ,`b_site.b_visit_office_id as office` , `doc.employee_lastname `)
+    ,`doc.employee_firstname` , `doc.employee_lastname `)
     .column(knex.raw('substr(t_health_epi_detail.record_date_time,1,10) as recdate'))
+   
     .leftJoin('b_health_epi_group','t_health_epi_detail.b_health_epi_set_id','=','b_health_epi_group.b_health_epi_group_id')
+    .leftJoin('b_employee as doc','t_health_epi_detail.health_epi_detail_staff_record','=','doc.b_employee_id')
+    
     .where('t_health_epi_detail.health_epi_detail_active','1')
        .where('t_health_epi_detail.t_patient_id',id)
       .orderBy('t_health_epi_detail.record_date_time', 'asc')
